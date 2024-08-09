f7b57c9386c7f0f2507493b897a643a2ab9fdbf9
nguyenlong03 <nguyenlong682003@gmail.com>
2024-08-10 00:39:19 +0700

    sửa code conten story

--- src/components/content/contentFeed/ContentFeed.js
+++ src/components/content/contentFeed/ContentFeed.js
@@ -1,8 +1,21 @@
 import React from "react";
 import "../contentFeed/ContentFeed.scss";
+import ContenFeedStory from "./contenFeedStory/contenFeedStory";
 
 function ContentFeed(props) {
-  return <div className="feed-main">hello NavbarFeed</div>;
+  return (
+    <div className="feed-main">
+      <div className="feed-str">
+       <ContenFeedStory/>
+      </div>
+      <div className="feed-content">
+
+      </div>
+      <div className="feed-post">
+
+      </div>
+    </div>
+  )
 }
 
 export default ContentFeed;
